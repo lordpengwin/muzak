83a8444e51d1a4b0176e2ee889c5102e008f0b3a
MikeDeSantis <mikedesantis608@gmail.com>
2016-12-02 11:11:17 -0500

    Removed extra info
    Removed a customized config.js

--- config.js
+++ config.js
@@ -4,20 +4,14 @@ var config = {};
 
 // Set the Alexa API Application ID to control access
 
-config.alexaAppID = "amzn1.ask.skill.ca688ca9-1280-4c60-8ce3-f0b0cac4217c";
-//AWS Lambda ARN (North America):  arn:aws:lambda:us-east-1:027416346470:function:squeezebox
-
-//Function:
-//Runtime Node.js 4.3
-//Handler: muzak.handler
-//Existing role: servied-role/lambda_basic_execution
+config.alexaAppID = "amzn1.echo-sdk-ams.app.XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX";
 
 // The connection properties for the squeezebox server. This server must be accessible from the Internet so it should
 // be protected by basic authentication. If it is not protected the username and password can be null.
 
-config.squeezeserverURL = "http://mikedesantis.homenet.org";
+config.squeezeserverURL = "http://yourdomain.com";
 config.squeezeserverPort = 9000;
-config.squeezeServerUsername = "mikedesantis";
-config.squeezeServerPassword = "ZtjXF28dq$zJEsvd6jFNWU4hnFr$PC";
+config.squeezeServerUsername = "your username";
+config.squeezeServerPassword = "your password";
 
 module.exports = config;
